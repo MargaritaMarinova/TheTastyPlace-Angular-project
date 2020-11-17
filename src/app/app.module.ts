3ea20b626e0064e50214f2e2e1eb37684fca1427
MargaritaMarinova <margarita.tomova@gmail.com>
2020-11-17 15:57:26 +0200

    create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeCreateComponent } from './recipes/recipe-create/recipe-create.component';
+import { UsersComponent } from './users/users.component';
+import { UserLoginComponent } from './users/user-login/user-login.component';
+import { UserRegisterComponent } from './users/user-register/user-register.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailsComponent,
+    RecipeEditComponent,
+    RecipeItemComponent,
+    RecipeCreateComponent,
+    UsersComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
