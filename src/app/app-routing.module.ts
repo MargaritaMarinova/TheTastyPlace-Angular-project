244fbd0ed9e9270dc6c237770cf9ef6c28fb990f
MargaritaMarinova <margarita.tomova@gmail.com>
2020-11-18 18:13:46 +0200

    add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { UserLoginComponent } from './users/user-login/user-login.component';
+import { UserRegisterComponent } from './users/user-register/user-register.component';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'register', component: UserRegisterComponent},
+  {path: 'login', component: UserLoginComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
