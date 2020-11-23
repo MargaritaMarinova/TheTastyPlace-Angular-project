8fcd62e70dad3fe5a46ce7462e7340c968c6e770
MargaritaMarinova <margarita.tomova@gmail.com>
2020-11-23 12:30:15 +0200

    Add basic Recipe Create form

--- src/app/recipes/recipe-create/recipe-create.component.ts
+++ src/app/recipes/recipe-create/recipe-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-recipe-create',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-create.component.css']
 })
 export class RecipeCreateComponent implements OnInit {
+  recipeForm: FormGroup;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm)
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeDescription = '';
+    let recipeCategory = '';
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imageUrl': new FormControl(recipeImageUrl),
+      'description': new FormControl(recipeDescription),
+      'category': new FormControl(recipeCategory)
+    });
+
   }
 
 }
