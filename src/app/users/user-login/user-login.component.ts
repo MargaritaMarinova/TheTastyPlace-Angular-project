099ce860d8df62fe71e1d869ccc161bf06510397
MargaritaMarinova <margarita.tomova@gmail.com>
2020-11-28 10:16:50 +0200

    Add error handling in Login

--- src/app/users/user-login/user-login.component.ts
+++ src/app/users/user-login/user-login.component.ts
@@ -8,19 +8,28 @@ import { UserService } from '../user.service';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent {
+  isLoading = false;
+  error: string = null;
 
   constructor(private userService: UserService) { }
 
   onSubmit(form: NgForm) {
+    this.isLoading = true;
     if (!form.valid) {
       return;
     }
-
     const email = form.value.email;
     const password = form.value.password;
     this.userService.login(email, password)
     .subscribe((resData) => {
       console.log(resData);
+      this.isLoading = false;
+    },
+    (errorMessage) => {
+      console.log(errorMessage);
+      this.error = errorMessage;
+
+      this.isLoading = false;
     });
     form.reset();
   }
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError } from 'rxjs/operators';
-import { throwError} from 'rxjs';
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 interface UserResponseData {
   idToken: string;
@@ -9,6 +9,7 @@ interface UserResponseData {
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: "root" })
@@ -16,35 +17,59 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   register(email: string, password: string) {
-    return this.http.post<UserResponseData>(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbwkUb-f5aOaG9kSryXxlZN9fFDZ2_aus",
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    ).pipe(catchError(errorRes=> {
-      let errorMessage = "Възникна неопределена грешка";
-      if(!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage);
-      }
-      switch(errorRes.error.error.message) {
-        case "EMAIL_EXISTS":
-          errorMessage = "Този мейл вече съществува"
-      }
+    return this.http
+      .post<UserResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbwkUb-f5aOaG9kSryXxlZN9fFDZ2_aus",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = "Възникна неопределена грешка";
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case "EMAIL_EXISTS":
+              errorMessage = "Този мейл вече съществува";
+          }
 
-      return throwError(errorMessage);
-    }));
+          return throwError(errorMessage);
+        })
+      );
   }
 
   login(email: string, password: string) {
-    return this.http.post<UserResponseData>(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAbwkUb-f5aOaG9kSryXxlZN9fFDZ2_aus",
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<UserResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAbwkUb-f5aOaG9kSryXxlZN9fFDZ2_aus",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          console.log(errorRes);
+          let errorMessage = "Възникна неопределена грешка";
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case "INVALID_PASSWORD":
+              errorMessage = "Невалидна парола";
+            case "EMAIL_NOT_FOUND":
+              errorMessage = "Този мейл не е регистриран";
+            case "USER_DISABLED":
+              errorMessage = "Този потребител е блокиран";
+          }
+
+          return throwError(errorMessage);
+        })
+      );
   }
 }
